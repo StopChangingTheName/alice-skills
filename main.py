9ceb331e5fbe8c6c06a392cd4007e11155203ff2
Артем (Desktop) <57802035+ArtyomRukavitsa@users.noreply.github.com>
2020-05-14 13:50:55 +0300

    started main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask, request
+import logging
+import json
+
+
+app = Flask(__name__)  # приложение
+logging.basicConfig(level=logging.INFO)  # логгируем данные
+sessionStorage = {}  # инфа о юзерах
+
+
+# Обработка пришедшей инфы Алисой
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info(f'Request: {request.json!r}')
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+
+    # Отправляем request.json и response в функцию handle_dialog.
+    # Она сформирует оставшиеся поля JSON, которые отвечают
+    # непосредственно за ведение диалога
+    handle_dialog(request.json, response)
+
+    logging.info(f'Response:  {response!r}')
+
+    # Преобразовываем в JSON и возвращаем
+    return json.dumps(response)
+
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Инициализируем сессию и поприветствуем его.
+        # Запишем подсказки, которые мы ему покажем в первый раз
+
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+        # Заполняем текст ответа
+        res['response']['text'] = 'Привет! Купи слона!'
+        # Получим подсказки
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+
+    # Сюда дойдем только, если пользователь не новый,
+    # и разговор с Алисой уже был начат
+    pass  # заглушка, будет функция ИГРЫ, то есть тестирования
+
+
+
+if __name__ == '__main__':
+    app.run()
