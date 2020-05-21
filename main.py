5572df39f379f8b93b93ced2977c5e058459aa57
Артем (Desktop) <57802035+ArtyomRukavitsa@users.noreply.github.com>
2020-05-21 12:49:24 +0300

    Сохранение очков в sessionStorage

--- main.py
+++ main.py
@@ -66,7 +66,10 @@ def handle_dialog(req, res):
             'id': 0,
             'lastQ': False,
             'mode': '',
-            'lastPic': False
+            'lastPic': False,
+            'test_count': 0,
+            'pic_count': 0,
+            'ter_count': 0
         }
         res['response']['text'] = 'Привет! Я помогу тебе подготовиться к ЕГЭ по истории. ' \
                                   'Какой режим ты хочешь выбрать: случайные даты или портреты исторических личностей?'
@@ -100,6 +103,7 @@ def handle_dialog(req, res):
             if sessionStorage[user_id]['test'][sessionStorage[user_id]['id'] - 1][
                 'answer'].lower() in req['request']['original_utterance'].lower():
                 res['response']['text'] = f"{random.choice(right)} {random.choice(_next)}: {res['response']['text']}"
+                sessionStorage[user_id]['test_count'] += 1
             else:
                 res['response'][
                     'text'] = f"{random.choice(wrong)} Правильный ответ: {sessionStorage[user_id]['test'][sessionStorage[user_id]['id'] - 1]['answer']}. \n{random.choice(_next)}: {res['response']['text']}"
@@ -123,6 +127,7 @@ def handle_dialog(req, res):
             if sessionStorage[user_id]['arrayPic'][sessionStorage[user_id]['idPic'] - 1].lower() \
                     in req['request']['original_utterance'].lower():
                 res['response']['card']['title'] = random.choice(right)
+                sessionStorage[user_id]['pic_count'] += 1
             else:
                 res['response']['card']['title'] \
                     = f"{random.choice(wrong)} Правильный ответ: {sessionStorage[user_id]['arrayPic'][sessionStorage[user_id]['idPic'] - 1]}."
@@ -137,6 +142,7 @@ def handle_dialog(req, res):
         sessionStorage[user_id]['idPic'] += 1
 
     if sessionStorage[user_id]['mode'] == 'термины':
+        sessionStorage[user_id]['ter_count'] += 1  # Сохранение очков по терминам
         pass
 
     # если в нашем запросе 'закрыть' заканчиваем сессию
