1f4ccbc428f4c37187f3dfe6c182bcc84a53f1a8
Daniel Khromov <danielkhromov@gmail.com>
2020-05-22 17:15:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,6 @@ def main():
             'end_session': False
         },
         'session_state': {
-            'nick': str
         },
     }
     handle_dialog(request.json, response)
@@ -101,12 +100,12 @@ def handle_dialog(req, res):
             'lastT': False,
             'terID': 0
         }
-        res['session_state']['nick'] = 'b'
+        res['session_state']['nick'] = '#'
         res['response']['text'] = 'Привет! Я помогу тебе подготовиться к ЕГЭ по истории ✨\n ' \
                                   'Введи свой никнейм для сохранения:'
         return
 
-    if res['session_state']['nick'] == '':
+    if req['state']['nick'] == '#':
         tag = str(random.randint(0, 10001))
         res['session_state']['nick'] = req['request']['original_utterance'] + "#" + tag
         res['response']['text'] = f'Приятно познакомиться! Твой ник с тэгом: {res["session_state"]["nick"]}\n' \
