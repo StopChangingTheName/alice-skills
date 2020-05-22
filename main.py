9020e7e956c3bc128fb21bcaf43b49a956e225b4
Daniel Khromov <danielkhromov@gmail.com>
2020-05-22 16:41:15 +0300

    remember nick in session start

--- main.py
+++ main.py
@@ -48,7 +48,9 @@ def main():
         'response': {
             'end_session': False
         },
-        'session_state': {}
+        'session_state': {
+
+        },
     }
     handle_dialog(request.json, response)
 
@@ -58,7 +60,6 @@ def main():
 
 
 def handle_dialog(req, res):
-    res['session_state']['value'] = 10
     user_id = req['session']['user_id']
     # если 1 раз
     if req['session']['new']:
@@ -101,20 +102,20 @@ def handle_dialog(req, res):
 
         res['response']['text'] = 'Привет! Я помогу тебе подготовиться к ЕГЭ по истории ✨\n ' \
                                   'Введи свой никнейм для сохранения!'
-        logging.info(req['request']['original_utterance'])
+        res['session_state']['nick'] = req['request']['original_utterance']
         return
 
-    if sessionStorage[user_id]['nick'] is None:
-        tag = str(random.randint(0, 10001))
-        sessionStorage[user_id]['nick'] = req['request']['original_utterance'] + "#" + tag
-        res['response']['text'] = f'Приятно познакомиться! Твой ник с тэгом: {sessionStorage[user_id]["nick"]}\n' \
-                                  'Я буду спрашивать у тебя случайную дату, картину или термин. ' \
-                                  'За каждый правильный ответ в любом режиме зачисляются очки, будь внимателен! 😁'
-        res['response']['buttons'] = [
-            {'title': suggest, 'hide': False}
-            for suggest in sessionStorage[user_id]['suggests']
-        ]
-        return
+    # if res['session_state']['nick'] is None:
+    #     tag = str(random.randint(0, 10001))
+    #     sessionStorage[user_id]['nick'] = req['request']['original_utterance'] + "#" + tag
+    #     res['response']['text'] = f'Приятно познакомиться! Твой ник с тэгом: {sessionStorage[user_id]["nick"]}\n' \
+    #                               'Я буду спрашивать у тебя случайную дату, картину или термин. ' \
+    #                               'За каждый правильный ответ в любом режиме зачисляются очки, будь внимателен! 😁'
+    #     res['response']['buttons'] = [
+    #         {'title': suggest, 'hide': False}
+    #         for suggest in sessionStorage[user_id]['suggests']
+    #     ]
+    #     return
 
     if 'меню' in req['request']['original_utterance'].lower():
         res['response']['text'] = 'Я буду спрашивать у тебя случайную дату, картину или термин. ' \
